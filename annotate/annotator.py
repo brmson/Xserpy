0388f9e4cfea933a09eb358315a7438600fa2fe3
Martin Matulík <matulma4@fel.cvut.cz>
2016-01-06 13:04:02 +0100

    Added json file loading

--- annotate/annotator.py
+++ annotate/annotator.py
@@ -1,6 +1,7 @@
-import argparse
+import argparse,json
 
 if __name__  == "__main__":
     parser = argparse.ArgumentParser(description="Annotate questions with DAGs")
     parser.add_argument("fpath",help="filepath",type=str)
     args = parser.parse_args()
+    a = json.load(open(args.fpath))
