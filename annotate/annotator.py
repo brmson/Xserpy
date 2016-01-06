d0984926e0956266dd33cac0cf07df7eeab3b0ea
Martin Matulík <matulma4@fel.cvut.cz>
2016-01-06 13:08:24 +0100

    Added json file loading to objects

--- annotate/annotator.py
+++ annotate/annotator.py
@@ -1,7 +1,15 @@
 import argparse,json
 
+class Question(object):
+    def __init__(self, utterance, targetFormula):
+        self.utterance = utterance
+        self.targetFormula = targetFormula
+
+def object_decoder(obj):
+    return Question(obj['utterance'], obj['targetFormula'])
+
 if __name__  == "__main__":
     parser = argparse.ArgumentParser(description="Annotate questions with DAGs")
     parser.add_argument("fpath",help="filepath",type=str)
     args = parser.parse_args()
-    a = json.load(open(args.fpath))
+    questions = json.load(open(args.fpath),object_hook=object_decoder)
