0c0abe4b3d7bee79d566f6ef18365eaaafa3acae
Martin Matulík <matulma4@fel.cvut.cz>
2016-01-06 13:17:44 +0100

    Added function for user to annotate questions with DAGs, with modifiable index of starting question

--- annotate/annotator.py
+++ annotate/annotator.py
@@ -8,8 +8,20 @@ class Question(object):
 def object_decoder(obj):
     return Question(obj['utterance'], obj['targetFormula'])
 
+def annotate_questions(questions):
+    for q in questions:
+        M = []
+        print q.utterance
+        l = q.utterance.split()
+        for word in l:
+            edges = raw_input(word)
+            M.append([int(e) for e in edges.split()])
+        print M
+
 if __name__  == "__main__":
     parser = argparse.ArgumentParser(description="Annotate questions with DAGs")
     parser.add_argument("fpath",help="filepath",type=str)
+    parser.add_argument("start",help="start",type=int,default=0)
     args = parser.parse_args()
     questions = json.load(open(args.fpath),object_hook=object_decoder)
+    annotate_questions(questions[args.start:])
