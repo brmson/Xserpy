f33fa370c016d1de6a7b971f8dfccd3f9db8e693
Martin Matulík <matulma4@fel.cvut.cz>
2016-01-10 12:28:51 +0100

    Added method for NER tagging of questions

--- annotate/annotator.py
+++ annotate/annotator.py
@@ -1,4 +1,5 @@
-import argparse,json
+import argparse,json,nltk,os
+from nltk.tag import StanfordNERTagger,StanfordPOSTagger
 
 class Question(object):
     def __init__(self, utterance, targetFormula):
@@ -8,15 +9,30 @@ class Question(object):
 def object_decoder(obj):
     return Question(obj['utterance'], obj['targetFormula'])
 
-def annotate_questions(questions):
+def annotate_questions(questions,start):
+    i = start
     for q in questions:
         M = []
-        print q.utterance
+        print i
+        # print range(len(q.utterance.split()))
         l = q.utterance.split()
+        print list(enumerate(l))
         for word in l:
-            edges = raw_input(word)
+            edges = raw_input(word+" ")
             M.append([int(e) for e in edges.split()])
         print M
+        i += 1
+
+def ner_tag(questions):
+    path = 'C:\\Users\Martin\\PycharmProjects\\xserpy\\stanford-nlp\\'
+    st_ner = StanfordNERTagger(path+'classifiers\\english.all.3class.distsim.crf.ser.gz',path+'stanford-ner.jar')
+    java_path = "C:\\Program Files\\Java\\jdk1.8.0_65\\bin\\java.exe"
+    os.environ['JAVAHOME'] = java_path
+    tagged = []
+    for q in questions:
+        text = nltk.word_tokenize(q.utterance)
+        tagged.append(st_ner.tag(text))
+    return tagged
 
 if __name__  == "__main__":
     parser = argparse.ArgumentParser(description="Annotate questions with DAGs")
@@ -24,4 +40,4 @@ if __name__  == "__main__":
     parser.add_argument("start",help="start",type=int,default=0)
     args = parser.parse_args()
     questions = json.load(open(args.fpath),object_hook=object_decoder)
-    annotate_questions(questions[args.start:])
+    # annotate_questions(questions[args.start:],args.start)
