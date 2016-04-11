6375aa04852092b93b0bda28c7fe2b889a765ecb
Martin Matulík <matulma4@fel.cvut.cz>
2016-04-11 14:49:07 +0200

    Added script to create csv of features; Added testing pickles

---
+++ feature_processor.py
@@ -0,0 +1,46 @@
+import pickle,numpy as np
+from sklearn.preprocessing import Imputer,OneHotEncoder
+
+def process_features(ftr):
+    features = {}
+    i = 0
+    for F in ftr:
+        for feature in F:
+            f = feature.split('.')
+            if f[0] not in features.keys():
+                features[f[0]] = ['x' for j in range(len(ftr))]
+            features[f[0]][i] = f[1]
+        i += 1
+    return np.matrix([process_row(features[x]) for x in features.keys()]).T
+
+def process_row(row):
+    unique = list(set(row))
+    dct = dict([(item,index) for index,item in list(enumerate(unique))])
+    result = [mapper(x,dct) for x in row]
+    return result
+
+def mapper(x, dct):
+    if x == 'x':
+        return np.nan
+    return dct[x]
+
+def imputator(features):
+    imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    imp.fit(features)
+    return imp.transform(features)
+
+def encode(features, labels):
+    enc = OneHotEncoder()
+    enc.fit(features)
+    arr = enc.transform(features).toarray()
+    result = np.array([[0 for j in range(len(arr[0])+1)] for i in range(len(arr))])
+    for i in range(len(arr)):
+        result[i] = np.append(arr[i], labels[i])
+    return result
+
+if __name__ == "__main__":
+
+    examples = pickle.load(open("dag_labs_tst_276.pickle"))
+    ftr = zip(*examples)
+    features = encode(imputator(process_features(ftr[0])),ftr[1])
+    np.savetxt("features.csv", features, delimiter=",")
