75d91c0bd941012888e1862987fbe201b839d9aa
Martin Matulík <matulma4@fel.cvut.cz>
2016-04-21 14:05:25 +0200

    Parametrized file

--- convert_question.py
+++ convert_question.py
@@ -1,28 +1,53 @@
 import pickle
-from phrase_detector import predict
-from annotate.annotator import object_decoder, Question, json, parse_to_phrases
-import structured_query, shift_reduce, query_instantiate
+
+from phrase_detection.phrase_detector import predict
+from annotate.annotator import object_decoder, json
+import structured_query, os, argparse
+from shift_reduce import shift_reduce
+from query_intention import query_instantiate
+
 
 def convert_question(phrase, dag, qint, question, features, pos_tagged):
     phrases = []
+
     for U in features:
         phrases.append(predict(phrase, U, 4))
     phr, pos = shift_reduce.parse_to_phrases([question], [phrases], [pos_tagged])
+
     DAG = shift_reduce.shift_reduce(phr[0], pos[0], dag, 50).dag
+
     examples, features = query_instantiate.create_examples([question], phr, [DAG])
     rel_entities, simple = query_instantiate.get_db_entities([question])
     relations, entities = query_instantiate.get_entities_relations(rel_entities)
+
     intent = query_instantiate.beam_search(examples[0], 30, ['x']+relations, qint, training=False)
-    structured_query.create_query_file("query.txt", structured_query.convert_to_queries(intent, phr[0]))
+
+    structured_query.create_query_file("query.txt", structured_query.convert_to_queries(intent, phr[0]), phr)
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Train weights for detecting query intention or create gold standard")
+    parser.add_argument("size", help="Dataset size", type=int, default=0)
+    parser.add_argument("i", help="Index", type=int, default=0)
+    parser.add_argument("n_iter", help="Number of iterations", type=int, default=0)
+    parser.add_argument("n_iter_dag", help="Number of iterations for DAG training", type=int, default=0)
+    parser.add_argument("mode", help="mode", type=str)
+    args = parser.parse_args()
+    size = args.size
+    i = args.i
+    n_iter = args.n_iter
+    n_iter_dag = args.n_iter_dag
+    mode = args.mode
+
+    sep = os.path.sep
     path = "C:\\Users\\Martin\\PycharmProjects\\xserpy\\"
-    pos = pickle.load(open("data\\pos_tagged.pickle"))
-    questions = json.load(open(path+"data\\free917.train.examples.canonicalized.json"), object_hook=object_decoder)
-    features = pickle.load(open(path + "annotate\\phrase_detect_features_100_arr.pickle"))
-    model_phrase = pickle.load(open(path+"models\\w_all_50.pickle"))
-    model_dag = pickle.load(open(path+"models\\w_dag_all.pickle"))
-    model_qint = pickle.load(open(path+"models\\w_qint.pickle"))
-    i = 0
+
+    pos = pickle.load(open("data" + sep + "pos_tagged_" + mode + ".pickle"))
+    questions = json.load(open(path+"data" + sep + "free917." + mode + ".examples.canonicalized.json"), object_hook=object_decoder)
+    features = pickle.load(open(path + "data" + sep + "phrase_detect_features_" + mode + "_" + str(size) + "_arr.pickle"))
+    model_phrase = pickle.load(open(path+"models" + sep + "w_" + str(size) + "_" + str(n_iter) + ".pickle"))
+    model_dag = pickle.load(open(path+"models" + sep + "w_dag" + str(size) + "_i" + str(n_iter_dag) + ".pickle"))
+    model_qint = pickle.load(open(path+"models" + sep + "w_qint.pickle"))
+
     u = len(questions[i].utterance.split())
     convert_question(model_phrase, model_dag, model_qint, questions[i], features[i:i+u], pos[i])
