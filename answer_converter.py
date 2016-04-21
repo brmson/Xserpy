b0f48d22f7f6e78ae3d9c24c5aca38910b1cbc0a
Martin Matulík <matulma4@fel.cvut.cz>
2016-04-21 14:30:47 +0200

    Added answers for testing split of questions; Credit to https://github.com/pks/rebol

--- answer_converter.py
+++ answer_converter.py
@@ -3,15 +3,15 @@ import json
 
 if __name__ == "__main__":
     path = "C:\\Users\\Martin\\PycharmProjects\\xserpy\\"
-    questions = json.load(open(path+"data\\free917.train.examples.canonicalized.json"), object_hook=object_decoder)
-    answers = [line.strip() for line in open("free917v2.dev.gold")]
-    formulas = [line.strip() for line in open("free917v2.dev.mrl")]
+    questions = json.load(open(path+"data\\free917.tst.examples.canonicalized.json"), object_hook=object_decoder)
+    answers = [line.strip() for line in open("data\\free917v2.test.gold")]
+    formulas = [line.strip() for line in open("data\\free917v2.test.mrl")]
     z = zip(formulas,answers)
     gold = dict(zip(formulas,answers))
-    with open("free917_trn_answers.txt","w") as f:
+    with open("data\\free917_tst_answers.txt","w") as f:
         for q in questions:
             if q.targetFormula in gold.keys():
-                print(gold[q.targetFormula])
                 f.write(gold[q.targetFormula]+"\n")
             else:
+                print(q.utterance)
                 f.write("\n")
