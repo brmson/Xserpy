7bfb4440a39a729204c4dfe6849cdcf788d2553f
Martin Matulík <matulma4@fel.cvut.cz>
2016-04-28 11:32:45 +0200

    Added method for querying Freebase for properties of an entity; Removed unused code

--- query_intention/fb_query.py
+++ query_intention/fb_query.py
@@ -1,28 +1,39 @@
 import json
 import urllib
 
-def query_freebase(query, scoring, size, id, category=False):
+def query_freebase_entity(query, scoring, size):
     api_key = open(".api_key").read()
     service_url = 'https://www.googleapis.com/freebase/v1/search'
     params = {
             'query': query,
             'key': api_key,
             'scoring': scoring,
-            'lang' : 'en',
-            #'type' : '/type/type',
-            #'with' : 'commons'
+            'lang' : 'en'
     }
-    if category:
-        params['type'] = '/type/property'
     url = service_url + '?' + urllib.urlencode(params)
     response = json.loads(urllib.urlopen(url).read())
-    result = None
+    result = []
     for r in response['result'][:size]:
-        if 'id' in r.keys():
-            if id in r['id']:
-                result = r
-                break
+        result.append(r)
+    return result
+
+def query_freebase_property(topic_id):
+    api_key = open(".api_key").read()
+    service_url = 'https://www.googleapis.com/freebase/v1/topic'
+    params = {
+      'key': api_key,
+      'filter': 'allproperties'
+    }
+    url = service_url + topic_id + '?' + urllib.urlencode(params)
+    topic = json.loads(urllib.urlopen(url).read())
+    result = []
+    for property in topic['property']:
+        if property[:5] != '/type':
+            result.append(property)
     return result
 
 if __name__ == "__main__":
-    print query_freebase('Marshall Hall', 'freebase', 10,'/en/marshall_hall',)
+    entity = query_freebase_entity("Beyonce",'freebase', 5)[0]
+    candidates = query_freebase_property(entity['mid'])
+    for c in candidates:
+        print c
