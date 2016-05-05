79a5b73b85ce95bd66d8d3867b4ce267d7220b69
Martin Matulík <matulma4@fel.cvut.cz>
2016-05-05 12:51:36 +0200

    Added method for obtaining property candidates from Freebase endpoint via SPARQL

--- query_intention/fb_query.py
+++ query_intention/fb_query.py
@@ -1,5 +1,6 @@
 import json
 import urllib
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 def query_freebase_entity(query, scoring, size):
     api_key = open(".api_key").read()
@@ -32,8 +33,16 @@ def query_freebase_property(topic_id):
             result.append(property)
     return result
 
+def query_fb_property_sparql(mid):
+    sparql = SPARQLWrapper('http://freebase.ailao.eu:3030/freebase/query')
+    sparql.setReturnFormat(JSON)
+    m = mid.split('/')[-1]
+    sparql.setQuery("PREFIX : <http://rdf.freebase.com/ns/>\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\nSELECT ?x {\n:m." + m + " :type.object.type ?x}")
+    results = sparql.query().convert()['results']['bindings']
+    return [r['x']['value'][27:] for r in results]
+
 if __name__ == "__main__":
     entity = query_freebase_entity("Beyonce",'freebase', 5)[0]
-    candidates = query_freebase_property(entity['mid'])
+    candidates = query_fb_property_sparql(entity['mid'])
     for c in candidates:
         print c
