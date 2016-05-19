47324f08a92c8f48d1005729dbaf24ce663a2b10
Martin Matulík <matulma4@fel.cvut.cz>
2016-05-19 10:31:38 +0200

    Added documentation; Fixed condition for entities; Parametrized

--- question_conversion/structured_query.py
+++ question_conversion/structured_query.py
@@ -1,7 +1,13 @@
-import pickle
+import pickle, sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 def convert_to_queries(dag):
+    """Convert labeled DAG to list of strings representing SPARQL WHERE conditions
+
+    Keyword arguments:
+    dag -- DAG linked to knowledge base
+
+    """
     dct = {30: 5, 20: 4, 12: 3, 6: 2}
     queries = get_entity_names(dag)
     for d in range(len(dag)):
@@ -39,7 +45,15 @@ def convert_to_queries(dag):
                         queries.append(query)
     return queries
 
-def create_query_file(filename, queries,phr):
+def create_query_file(filename, queries, phr):
+    """Write SPARQL query to a file
+
+    Keyword arguments:
+    filename -- name of output file
+    queries -- list of condition strings
+    phr -- words of question grouped by phrase labels
+
+    """
     f = open(filename, "w")
     f.write("PREFIX : <http://rdf.freebase.com/ns/>\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n")
     select = "?x"
@@ -58,6 +72,13 @@ def create_query_file(filename, queries,phr):
 
 
 def create_query(queries, phr):
+    """Create query as a string
+
+    Keyword arguments:
+    queries -- list of condition strings
+    phr -- words of question grouped by phrase labels
+
+    """
     f = ''
     f += "PREFIX : <http://rdf.freebase.com/ns/>\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
     select = "?x"
@@ -79,19 +100,30 @@ def create_query(queries, phr):
 
 
 def get_entity_names(dag):
+    """Add conditions to determine machine ID when only human-readable ID is known
+
+    Keyword arguments:
+    dag -- DAG linked to knowledge base
+
+    """
     alphabet = ['?a','?b','?c','?d','?e','?f','?g','?h','?i']
     index = 0
     result = []
     for i in range(len(dag)):
         d = dag[i]
-        if d != 'x':
-            if d[:2] == 'en':
-                dag[i] = alphabet[index]
-                result.append(alphabet[index]+" rdfs:label \""+d[3:].replace('_',' ').title()+"\"@en")
-                index += 1
+        if d[:3] == 'en.':
+            dag[i] = alphabet[index]
+            result.append(alphabet[index]+" rdfs:label \""+d[3:].replace('_',' ').title()+"\"@en")
+            index += 1
     return result
 
 def query_fb_endpoint(query):
+    """Send request for data to Freebase endpoint
+
+    Keyword arguments:
+    query -- query in string representation
+
+    """
     sparql = SPARQLWrapper('http://freebase.ailao.eu:3030/freebase/query')
     sparql.setReturnFormat(JSON)
     sparql.setQuery(query)
@@ -99,10 +131,10 @@ def query_fb_endpoint(query):
 
 
 if __name__ == "__main__":
-    dags = pickle.load(open("C:\\Users\\Martin\\PycharmProjects\\xserpy\\query_intention\\query_gold_int_trn.pickle"))
+    dags = pickle.load(open(sys.argv[2]))
     phr = pickle.load(open("phrases_100.pickle"))
 
-    i = 52
+    i = sys.argv[1]
     q = []
     for d in dags[i:i+1]:
         q.append(convert_to_queries(d))
